cbf33eed863267418ce3b4f6f4fc06cdfe3c7ce0
emmanuel mawutor <emawutor@indiana.edu>
2018-09-09 20:08:31 -0700

    added a navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-      value: this.props.counter.value,
-      /*tags: ['tag1', 'tag2', 'tag3']*/
-    };
+  /*state = {
+        value: this.props.counter.value,
+        tags: ['tag1', 'tag2', 'tag3']
+      };*/
 
     styles = {
       fontSize: 10,
       fontWeight: "bold"
     }
 
-    handleIncrement = () => {
-      this.setState( { value: this.state.value + 1});
-    }
+    /*handleIncrement = () => {
+          this.setState( { value: this.state.value + 1});
+        parent will handle this state*/
 
   render() {
     {/*constructor() {
@@ -23,7 +23,7 @@ class Counter extends Component {
     return (
       <div>
         <span style={ this.styles } className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className= "btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className= "btn btn-secondary btn-sm">Increment</button>
         <button onClick={() => this.props.onDelete(this.props.counter.id)} className= "btn btn-danger btn-sm m-2">Delete</button>
       {/*<ul>
               {this.state.tags.map(tag =>
@@ -38,12 +38,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state
+    const { value } = this.props.counter
     return value === 0 ? 'Zero' : value;
   }
 }
@@ -54,4 +54,5 @@ export default Counter;
 {/* each map should have a key that is unique, add an id name or object to key */}
 {/*using arraw function is cleaner and simpler than using a constructor*/}
 {/*in react you have to explicitly tell it what has changes using this.setState*/}
-{/*the companent with the event shold be the one modifying the state of the event so if you want to call a method to change the state you must raise onDelete*/}
+
+{/*controlled component doesnt have its own local state, recieves state from its parent via props*/}
